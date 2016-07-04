8e03f7206b7cc4f17a20fb5888d80d158cfec478
Danil Pismenny <danil@brandymint.ru>
2016-07-04 16:07:37 +0300

    reverse transaction

--- sql/8_migration_reverse_transaction.sql
+++ sql/8_migration_reverse_transaction.sql
@@ -35,3 +35,49 @@ END
 
 $restrict_transaction$ LANGUAGE plpgsql;
 
+
+CREATE OR REPLACE FUNCTION process_account_transaction() RETURNS TRIGGER AS $process_transaction$
+BEGIN
+  -- У всех счетов и транзакции должна быть одинаковая валюта
+
+  IF NEW.operation_id IS NOT NULL THEN
+    PERFORM * FROM OPENBIL_OPERATIONS WHERE ID = NEW.operation_id AND from_account_id = NEW.from_account_id AND to_account_id = NEW.to_account_id;
+    IF NOT FOUND THEN
+      RAISE EXCEPTION 'Operation (#%) has wrong accounts', NEW.operation_id;
+    END IF;
+
+  END IF;
+
+  PERFORM * FROM OPENBILL_ACCOUNTS where id = NEW.from_account_id and amount_currency = NEW.amount_currency;
+  IF NOT FOUND THEN
+    RAISE EXCEPTION 'Account (from #%) has wrong currency', NEW.from_account_id;
+  END IF;
+
+  PERFORM * FROM OPENBILL_ACCOUNTS where id = NEW.to_account_id and amount_currency = NEW.amount_currency;
+  IF NOT FOUND THEN
+    RAISE EXCEPTION 'Account (to #%) has wrong currency', NEW.to_account_id;
+  END IF;
+
+  IF NEW.reverse_transaction_id IS NOT NULL THEN
+    PERFORM * FROM openbill_transactions
+      WHERE amount_cents = NEW.amount_cents 
+        AND amount_currency = NEW.amount_currency 
+        AND from_account_id = NEW.to_account_id
+        AND to_account_id = NEW.from_account_id
+        AND id = NEW.reverse_transaction_id;
+
+    IF NOT FOUND THEN
+      RAISE EXCEPTION 'Not found reverse transaction with same accounts and amount (#%)', NEW.reverse_transaction_id;
+    END IF;
+
+  END IF;
+
+  -- установить last_transaction_id, counts и _at
+  UPDATE OPENBILL_ACCOUNTS SET amount_cents = amount_cents - NEW.amount_cents, last_transaction_id = NEW.id, last_transaction_at = NEW.created_at, transactions_count = transactions_count + 1 WHERE id = NEW.from_account_id;
+  UPDATE OPENBILL_ACCOUNTS SET amount_cents = amount_cents + NEW.amount_cents, last_transaction_id = NEW.id, last_transaction_at = NEW.created_at, transactions_count = transactions_count + 1 WHERE id = NEW.to_account_id;
+
+  return NEW;
+END
+
+$process_transaction$ LANGUAGE plpgsql;
+
