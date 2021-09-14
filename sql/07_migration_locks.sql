41a9f2839b88c11db5317bb77db4c622a94337e5
Vladimir Ivanov <procurator@bitzlato.com>
2021-09-14 09:32:10 +0300

    Add migration for table OPENBILL_LOCKS. Changed the names of the migration files

---
+++ sql/07_migration_locks.sql
@@ -0,0 +1,19 @@
+CREATE TABLE OPENBILL_LOCKS (
+  id              UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  owner_id        UUID,
+  username        character varying(255) not null,
+  date            date default current_date not null,
+  created_at      timestamp without time zone default current_timestamp,
+  account_id uuid not null,
+  amount_cents    numeric not null,
+  amount_currency character varying(8) not null,
+  key             character varying(256) UNIQUE not null,
+  details         text not null,
+  meta            hstore not null default ''::hstore,
+  lock_key   character varying(256) not null,
+  foreign key (lock_key) REFERENCES OPENBILL_LOCKS (key) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  foreign key (account_id) REFERENCES OPENBILL_ACCOUNTS (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+);
+
+CREATE UNIQUE INDEX index_locks_on_key ON OPENBILL_LOCKS USING btree (key);
+CREATE INDEX index_locks_on_meta ON OPENBILL_LOCKS USING gin (meta);
