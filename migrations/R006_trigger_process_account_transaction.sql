139c47ac002abf19cffd09fa067512b2c9aef7f2
Vladimir Ivanov <89069981+vivanov-bitzlato@users.noreply.github.com>
2021-10-26 16:02:19 +0300

    [#32] row locks (#39)

--- migrations/R006_trigger_process_account_transaction.sql
+++ migrations/R006_trigger_process_account_transaction.sql
@@ -2,15 +2,14 @@ CREATE OR REPLACE FUNCTION process_account_transaction() RETURNS TRIGGER AS $pro
 DECLARE
  v_account_from OPENBILL_ACCOUNTS%rowtype;
 BEGIN
-  SELECT * FROM OPENBILL_ACCOUNTS WHERE id = NEW.from_account_id INTO v_account_from;
+  SELECT * FROM OPENBILL_ACCOUNTS WHERE id = NEW.from_account_id FOR UPDATE INTO v_account_from;
   -- У всех счетов и транзакции должна быть одинаковая валюта
 
   IF NEW.amount_currency <> v_account_from.amount_currency THEN
     RAISE EXCEPTION 'Account (from #%) has wrong currency', NEW.from_account_id;
   END IF;
 
-  PERFORM * FROM OPENBILL_ACCOUNTS where id = NEW.to_account_id and amount_currency = NEW.amount_currency;
-  IF NOT FOUND THEN
+  IF NOT EXISTS (SELECT * FROM OPENBILL_ACCOUNTS where id = NEW.to_account_id and amount_currency = NEW.amount_currency) THEN
     RAISE EXCEPTION 'Account (to #%) has wrong currency', NEW.to_account_id;
   END IF;
 
