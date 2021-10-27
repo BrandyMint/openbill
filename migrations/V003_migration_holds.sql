ec61b17e2dcbfbacd38e58bf1b607f776dd3d71a
Vladimir Ivanov <procurator@bitzlato.com>
2021-10-27 17:23:58 +0300

    fix: repared all test

--- migrations/V003_migration_holds.sql
+++ migrations/V003_migration_holds.sql
@@ -1,17 +1,20 @@
 CREATE TABLE OPENBILL_HOLDS (
   id              UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  date            date default current_date not null,
   created_at      timestamp without time zone default current_timestamp,
   account_id uuid not null,
   amount_value    numeric(36,18) not null,
   amount_currency character varying(8) not null default 'USD',
+  remote_idempotency_key             character varying(256) UNIQUE not null,
   details         text not null,
   meta            jsonb not null default '{}'::jsonb,
-  hold_id   uuid,
-  foreign key (hold_id) REFERENCES OPENBILL_HOLDS (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  hold_key   character varying(256),
+  foreign key (hold_key) REFERENCES OPENBILL_HOLDS (remote_idempotency_key) ON DELETE RESTRICT ON UPDATE RESTRICT,
   foreign key (account_id) REFERENCES OPENBILL_ACCOUNTS (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CHECK ((amount_value < 0 AND hold_id is NOT NULL) or (amount_value >0 AND hold_id is NULL))
+  CHECK ((amount_value < 0 AND hold_key is NOT NULL) or (amount_value >0 AND hold_key is NULL))
 );
 
+CREATE UNIQUE INDEX index_holds_on_key ON OPENBILL_HOLDS USING btree (remote_idempotency_key);
 CREATE INDEX index_holds_on_meta ON OPENBILL_HOLDS USING gin (meta);
 
 ALTER TABLE openbill_accounts ADD COLUMN hold_value numeric(36,18) not null DEFAULT 0;
--- tests/test_account_incoming.sh
+++ tests/test_account_incoming.sh
@@ -6,13 +6,11 @@ echo "insert into OPENBILL_ACCOUNTS  (id, category_id) values ($ACCOUNT1_UUID, $
 echo "insert into OPENBILL_ACCOUNTS  (id, category_id) values ($ACCOUNT2_UUID, $CATEGORY_UUID)" | ./tests/sql.sh && \
 echo "insert into OPENBILL_ACCOUNTS  (id, category_id) values ($ACCOUNT3_UUID, $CATEGORY_UUID)" | ./tests/sql.sh && \
 
-./tests/assert_result_include.sh "insert into OPENBILL_TRANSACTIONS (invoice_id, amount_value, amount_currency, from_account_id, to_account_id, key, details) values ($INVOICE_UUID, 100, 'USD', $ACCOUNT2_UUID, $ACCOUNT1_UUID, 'gid://order3', 'test')" 'INSERT 0 1' && \
+./tests/assert_result_include.sh "insert into OPENBILL_TRANSACTIONS (amount_value, amount_currency, from_account_id, to_account_id, remote_idempotency_key, details) values ( 100, 'USD', $ACCOUNT2_UUID, $ACCOUNT1_UUID, 'gid://order3', 'test')" 'INSERT 0 1' && \
 
-./tests/assert_result_include.sh "select paid_value from OPENBILL_INVOICES where id =  $INVOICE_UUID" '100.000000000000000000' && \
+./tests/assert_result_include.sh "insert into OPENBILL_TRANSACTIONS ( amount_value, amount_currency, from_account_id, to_account_id, remote_idempotency_key, details) values (100, 'USD', $ACCOUNT2_UUID, $ACCOUNT3_UUID, 'gid://order4', 'test')" 'INSERT 0 1' && \
 
-./tests/assert_result_include.sh "insert into OPENBILL_TRANSACTIONS ( amount_value, amount_currency, from_account_id, to_account_id, key, details) values (100, 'USD', $ACCOUNT2_UUID, $ACCOUNT3_UUID, 'gid://order4', 'test')" 'INSERT 0 1' && \
-
-./tests/assert_result_include.sh "insert into OPENBILL_TRANSACTIONS ( amount_value, amount_currency, from_account_id, to_account_id, key, details) values (100, 'USD', $ACCOUNT3_UUID, $ACCOUNT1_UUID, 'gid://order5', 'test')" 'INSERT 0 1' && \
+./tests/assert_result_include.sh "insert into OPENBILL_TRANSACTIONS ( amount_value, amount_currency, from_account_id, to_account_id, remote_idempotency_key, details) values (100, 'USD', $ACCOUNT3_UUID, $ACCOUNT1_UUID, 'gid://order5', 'test')" 'INSERT 0 1' && \
 
 ./tests/assert_result.sh "update OPENBILL_TRANSACTIONS set amount_value=1" 'ERROR:  permission denied for table openbill_transactions' && \
 ./tests/assert_result.sh "delete from OPENBILL_TRANSACTIONS" 'ERROR:  permission denied for table openbill_transactions'
