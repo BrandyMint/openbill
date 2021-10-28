4bbb18e7882db2ef1fe91668b7a1fd829dfd5e91
Vladimir Ivanov <procurator@bitzlato.com>
2021-10-28 11:15:02 +0300

    comments: policy columns

--- migrations/V001_migration_policy.sql
+++ migrations/V001_migration_policy.sql
@@ -15,6 +15,10 @@ CREATE                TABLE OPENBILL_POLICIES (
 COMMENT ON TABLE OPENBILL_POLICIES IS 'Funds transfer policies. Using this table, you can restrict the movement of funds between accounts. For example, allow write-offs from user accounts only to system ones.';
 COMMENT ON COLUMN OPENBILL_POLICIES.id IS 'Policy unique id';
 COMMENT ON COLUMN OPENBILL_POLICIES.name IS 'Policy name';
+COMMENT ON COLUMN OPENBILL_POLICIES.from_category_id IS 'Category of accounts from which transfers are possible (NULL for all)';
+COMMENT ON COLUMN OPENBILL_POLICIES.to_category_id IS 'Category of accounts to which transfers are possible (NULL for all)';
+COMMENT ON COLUMN OPENBILL_POLICIES.to_account_id IS 'Fccounts to which transfers are possible (NULL for all)';
+COMMENT ON COLUMN OPENBILL_POLICIES.from_account_id IS 'Accounts from which transfers are possible (NULL for all)';
 
 CREATE UNIQUE INDEX index_openbill_policies_name ON OPENBILL_POLICIES USING btree (name);
 
