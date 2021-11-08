b96337cdecfa36bade88edf19dd6bcf98f2fcd8e
Vladimir Ivanov <procurator@bitzlato.com>
2021-11-08 18:40:52 +0300

    issue #38: add enum account kind for column kind

--- migrations/V009_account_type.sql
+++ migrations/V009_account_type.sql
@@ -1,7 +1,8 @@
-ALTER TABLE openbill_accounts ADD COLUMN kind smallint NOT NULL DEFAULT 3;
+CREATE TYPE account_kind AS ENUM ('negative', 'positive', 'any');
+ALTER TABLE openbill_accounts ADD COLUMN kind account_kind NOT NULL DEFAULT 'any';
 alter table openbill_accounts
-   add constraint openbill_accounts_kind_type0 check (kind = 0 AND amount_value>=0);
+   add constraint openbill_accounts_kind_type0 check (kind = 'positive' AND amount_value>=0);
    alter table openbill_accounts
-      add constraint openbill_accounts_kind_type1 check (kind = 1 AND amount_value<=0);
+      add constraint openbill_accounts_kind_type1 check (kind = 'negative' AND amount_value<=0);
 
-COMMENT ON COLUMN openbill_accounts.kind IS 'Тип аккаунта';
+COMMENT ON COLUMN openbill_accounts.kind IS 'Account type;
