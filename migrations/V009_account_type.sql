106d46c9987f28264b772b30453312333daf4965
Vladimir Ivanov <procurator@bitzlato.com>
2021-11-08 19:06:13 +0300

    fix: test & check

--- migrations/V009_account_type.sql
+++ migrations/V009_account_type.sql
@@ -1,8 +1,6 @@
 CREATE TYPE account_kind AS ENUM ('negative', 'positive', 'any');
 ALTER TABLE openbill_accounts ADD COLUMN kind account_kind NOT NULL DEFAULT 'any';
 alter table openbill_accounts
-   add constraint openbill_accounts_kind_positive check (kind = 'positive' AND amount_value>=0);
-   alter table openbill_accounts
-      add constraint openbill_accounts_kind_negative check (kind = 'negative' AND amount_value<=0);
+   add constraint openbill_accounts_kind check ( (kind = 'positive' AND amount_value >=0) OR (kind = 'negative' AND amount_value<=0) OR kind = 'any' );
 
 COMMENT ON COLUMN openbill_accounts.kind IS 'Account type';
